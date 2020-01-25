d3ffc03f0e4a1227f91aa2a24745e1ca4bef7ffd
Minush-usl <jn4ik2001@gmail.com>
2020-01-25 21:44:00 +0200

    routes secur

---
+++ src/app/admin/shared/services/auth.guard.ts
@@ -0,0 +1,36 @@
+import { Injectable } from "@angular/core";
+import { CanActivate } from '@angular/router/src/utils/preactivation';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable()
+
+export class AuthGuard implements CanActivate {
+
+    constructor( 
+        private auth: AuthService,
+        private router: Router
+        ) {}
+
+    path: ActivatedRouteSnapshot[];
+    route: ActivatedRouteSnapshot;
+
+    canActivate(
+        route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot
+    ): Observable<boolean> | Promise<boolean> | boolean {
+        if (this.auth.isAuthenticated()) {
+            return true
+        } else{
+            this.auth.logout()
+            this.router.navigate(['/admin', 'login'], {
+                queryParams: {
+                    loginAgain: true
+                }
+            })
+        }
+        
+    }
+
+}
