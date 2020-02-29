2fa85d68c31a2d8484460221520dd3b1bba378fe
Minush-usl <jn4ik2001@gmail.com>
2020-03-01 00:57:52 +0200

    post editing realization

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,19 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthService } from '../shared/services/auth.service';
+import { PostsService } from 'src/app/shared/posts.service';
+import { Post } from 'src/app/shared/interfaces';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent implements OnInit {
+export class DashboardPageComponent implements OnInit, OnDestroy {
+  posts: Post[] = []
+  pSub: Subscription 
+  dSub: Subscription 
+  searchStr: string = ''
 
-  constructor(private auth: AuthService) { }
+  constructor(private postsService: PostsService) { }
 
   ngOnInit() {
+    this.pSub = this.postsService.getAll().subscribe(posts => {
+      this.posts = posts
+    })
   }
 
-  test() {
-    console.log(this.auth.token)
+  remove(id: string) {
+    this.dSub =  this.postsService.remove(id).subscribe(() => {
+      this.posts = this.posts.filter(post => post.id !== id)
+    })
   }
+
+  ngOnDestroy() {
+    if (this.pSub) {
+      this.pSub.unsubscribe()
+    }
+
+    if (this.dSub) {
+      this.dSub.unsubscribe()
+    }
+  }
+
+
 }
+ 
--- src/app/shared/posts.service.ts
+++ src/app/shared/posts.service.ts
@@ -22,6 +22,40 @@ export class PostsService {
                     date: new Date(post.date)
                 }
             }))
+    }
+
+    getAll(): Observable<Post[]> {
+        return this.http.get(`${environment.fbDbUrl}/posts.json`)
+            .pipe(map((respose: { [key: string]: any }) => {
+                return Object
+                    .keys(respose)
+                    .map(key => ({
+                        ...respose[key],
+                        id: key,
+                        date: new Date(respose[key].date)
+                    }))
+            }))
 
     }
+
+    remove(id: string): Observable<void> {
+        return this.http.delete<void>(`${environment.fbDbUrl}/posts/${id}.json`)
+    }
+
+    getById(id: string): Observable<Post> {
+        return this.http.get<Post>(`${environment.fbDbUrl}/posts/${id}.json`)
+            .pipe(map((post: Post) => {
+                return {
+                    ...post, id,
+                    date: new Date(post.date)
+                }
+            }))
+    }
+
+    update(post: Post): Observable<Post> {
+        return this.http.patch<Post>(`${environment.fbDbUrl}/posts/${post.id}.json`, post)
+        
+    }
+    
+    
 } 
